c1f5a3b0139bcc7b7b10f684fbfece09a370895d
Simon Brüggen <simon.brueggen@trivago.com>
2022-12-07 13:36:25 +0100

    Add time-consuming functions

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,19 @@
 use actix_web::{get, App, HttpServer, Responder};
+use tokio::time::{sleep, Duration};
 
 #[get("/")]
 async fn greet() -> impl Responder {
+    get_from_db().await;
+    update_cache().await;
     "Hello World!"
+}
+
+async fn get_from_db() -> () {
+    sleep(Duration::from_millis(250)).await;
+}
+
+async fn update_cache() -> () {
+    sleep(Duration::from_millis(75)).await;
 }
 
 #[actix_web::main]
