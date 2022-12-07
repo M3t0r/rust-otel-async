f347ad350133b18b517290543ac495afc4e4c01e
Simon Brüggen <simon.brueggen@trivago.com>
2022-12-07 13:36:26 +0100

    Add sub-service request

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
-use actix_web::{get, App, HttpServer, Responder};
+use actix_web::{get, post, App, HttpServer, Responder};
 use tokio::time::{sleep, Duration};
 
 #[get("/")]
 async fn greet() -> impl Responder {
+    let _ms_resp = awc::Client::default()
+        .post("http://127.0.0.1:8080/microservice")
+        .send()
+        .await
+        .expect("requesting sub-service")
+        .body()
+        .await
+        .expect("reading response from sub-service");
+
     get_from_db().await;
     update_cache().await;
     "Hello World!"
@@ -16,12 +25,20 @@ async fn update_cache() -> () {
     sleep(Duration::from_millis(75)).await;
 }
 
+#[post("/microservice")]
+async fn microservice() -> impl Responder {
+    upsert_into_db().await;
+    "Hello microservice!"
+}
+
+async fn upsert_into_db() -> () {
+    sleep(Duration::from_millis(375)).await;
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new().service(greet)
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+    HttpServer::new(|| App::new().service(greet).service(microservice))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
